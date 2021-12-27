355c3e1028ea512f6b9914531eea11e65631b542
endisl <endiluamba@gmail.com>
2021-12-26 23:45:22 -0300

    Added system.properties

---
+++ src/main/java/com/endiluamba/creditmanager/config/SwaggerConfig.java
@@ -0,0 +1,44 @@
+package com.endiluamba.creditmanager.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    private final static String BASE_PACKAGE = "com.endiluamba.creditmanager";
+    private final static String API_TITLE = "Credit Manager";
+    private final static String API_DESCRIPTION = "Credit Manager API";
+    private final static String API_VERSION = "1.0.0";
+    private final static String CONTACT_NAME = "Endi Luamba";
+    private final static String CONTACT_GITHUB = "https://github.com/endisl";
+    private final static String CONTACT_EMAIL = "endiluamba@gmail.com";
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage(BASE_PACKAGE))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(buildApiInfo());
+    }
+
+    private ApiInfo buildApiInfo() {
+        return new ApiInfoBuilder()
+                .title(API_TITLE)
+                .description(API_DESCRIPTION)
+                .version(API_VERSION)
+                .contact(new Contact(CONTACT_NAME, CONTACT_GITHUB, CONTACT_EMAIL))
+                .build();
+    }
+}
--- src/main/java/com/endiluamba/creditmanager/controller/LoanController.java
+++ src/main/java/com/endiluamba/creditmanager/controller/LoanController.java
@@ -1,5 +1,8 @@
 package com.endiluamba.creditmanager.controller;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +11,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/loans")
 public class LoanController {
 
+    @ApiOperation(value = "Return a message test")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Correct return")
+    })
     @GetMapping
     public String hello() {
         return "Hello Credit Manager";
